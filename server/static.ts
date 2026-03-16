58fcb11e496c7bfad3b75f76f87ef77b7af9e7f8
sanularajapaksha-prog <sanularajapaksha@icloud.com>
2026-03-16 17:15:37 +0530

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -4,15 +4,14 @@ import path from "path";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
+    console.log("⚠️ No frontend build found. Skipping static serving.");
+    return;
   }
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("/{*path}", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
